0194fcc8d7ab5f0502027b6f4f615f3a0758e628
francops1722 <francops1722@gmail.com>
2025-11-24 10:06:11 +0100

    test job added

---
+++ test_jobs/Example_job_submission.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#PBS -N PeakPrime_job_
+#PBS -l nodes=1:ppn=3
+#PBS -l walltime=04:00:00
+#PBS -l mem=16gb
+
+#set the enviromental paths, make sure the folders exist beforehand
+export CONDA_PKGS_DIRS=/user/gent/446/vsc44685/ScratchVO_dir/conda_pkgs
+export NXF_CONDA_CACHEDIR=/user/gent/446/vsc44685/ScratchVO_dir/conda_cache
+
+# go to the pipeline directory
+cd path/to/peak/Primer_PeakFindR
+#load nextflow module
+ml Nextflow/25.04.4
+
+# run the complete pipeline with multipeak mode and primer optimization
+
+nextflow run main.nf --makeplots \
+  --bam /path/to/bamfile.bam \
+  --fasta /path/to/genome/GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+  --gtf /path/to/GTF/Homo_sapiens.GRCh38.109.gtf \
+  --genes ./path/to/geneidlist/TargetGeneIDs.txt \
+  --macs2_qvalue_threshold 0.1 \
+  --select_all_peaks \
+  --optimize_multipeak \
+  --transcriptome_index /path/to/pre-built/bowtie2_index \
+  --outdir ./results \
+  -profile pbs # to use the slurm scheduler of the HPC
+
+#if you have to only make plots after running the pipeline use the following 
+# some of these files are produced by the pipeline and should be found in the output directory
+nextflow run main.nf --makeplots \
+  --bw ./results/File.bw \
+  --gtf /path/to/GTF/Homo_sapiens.GRCh38.109.gtf \
+  --genes ./path/to/geneidlist/TargetGeneIDs.txt \
+  --peaks_tsv ./results/processed_peaks/selected_peaks.tsv \
+  --qc_tsv ./results/peaks_qc_summary.tsv \
+  --primer_targets_bed ./results/processed_peaks/primer_targets.bed \
+  --narrowpeak ./results/macs2_peaks/File.narrowPeak \
+  --outdir ./results/ \
+  -profile pbs
+
